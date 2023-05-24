1783ccfc74705baf5214a353bbd880f3f3693ce9
ZarmanRU <kampakcotsava@gmail.com>
2023-05-24 20:51:55 +0300

    laba
    labva

---
+++ TestSystem/Context/TestBdContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TestSystem.Models;
+
+namespace TestSystem.Context
+{
+    public class TestBdContext : DbContext
+    {
+        public TestBdContext(DbContextOptions<TestBdContext> options):base(options) 
+        {
+
+        }
+        public DbSet<Test> Tests { get; set; }
+    }
+
+}
---
+++ TestSystem/Models/Test.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace TestSystem.Models
+{
+    public class Test
+    {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Введите название теста")]
+        [Required(ErrorMessage = "Вам нужно ввести название")]
+        public string Name { get; set; }
+        [Display(Name="Введите описание")]
+        [StringLength(50, ErrorMessage = "Описание должно составлять менее 10 символов")]
+        [Required(ErrorMessage = "Вам нужно ввести описание")]
+        public string Description { get; set; } 
+
+       //public DateTime Date { get; set; }
+    }
+}
